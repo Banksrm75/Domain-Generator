ec2dcc29fba768078cf013c24ef2cfe80f766629
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-09-23 20:30:32 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
+    for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
+      for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
+        console.log(
+          pronoun[pronounIndex],
+          adj[adjIndex],
+          noun[nounIndex],
+          ".com"
+        );
+      }
+    }
+  }
 };
